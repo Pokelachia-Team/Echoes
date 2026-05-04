Rangi42/polishedcrystal

remove krabby line/add corphish line

gfx/minis_icons.asm
@@ -492,15 +492,15 @@ HypnoMini::     INCBIN "gfx/minis/hypno.2bpp.lz"
 HypnoMiniMask:: INCBIN "gfx/minis/hypno_mask.1bpp.lz"
 HypnoIcon::     INCBIN "gfx/icons/hypno.2bpp.lz"
 
-SECTION "Krabby Mini Icon", ROMX
-KrabbyMini::     INCBIN "gfx/minis/krabby.2bpp.lz"
-KrabbyMiniMask:: INCBIN "gfx/minis/krabby_mask.1bpp.lz"
-KrabbyIcon::     INCBIN "gfx/icons/krabby.2bpp.lz"
+SECTION "Corphish Mini Icon", ROMX
+CorphishMini::     INCBIN "gfx/minis/corphish.2bpp.lz"
+CorphishMiniMask:: INCBIN "gfx/minis/corphish_mask.1bpp.lz"
+CorphishIcon::     INCBIN "gfx/icons/corphish.2bpp.lz"
 
-SECTION "Kingler Mini Icon", ROMX
-KinglerMini::     INCBIN "gfx/minis/kingler.2bpp.lz"
-KinglerMiniMask:: INCBIN "gfx/minis/kingler_mask.1bpp.lz"
-KinglerIcon::     INCBIN "gfx/icons/kingler.2bpp.lz"
+SECTION "Crawdaunt Mini Icon", ROMX
+CrawdauntMini::     INCBIN "gfx/minis/crawdaunt.2bpp.lz"
+CrawdauntMiniMask:: INCBIN "gfx/minis/crawdaunt_mask.1bpp.lz"
+CrawdauntIcon::     INCBIN "gfx/icons/crawdaunt.2bpp.lz"
 
 SECTION "Voltorb Mini Icon", ROMX
 VoltorbMini::     INCBIN "gfx/minis/voltorb.2bpp.lz"

gfx/pokemon.asm
@@ -296,13 +296,13 @@ SECTION "Hypno Pics", ROMX
 HypnoFrontpic: INCBIN "gfx/pokemon/hypno/front.animated.2bpp.lz"
 HypnoBackpic:  INCBIN "gfx/pokemon/hypno/back.2bpp.lz"
 
-SECTION "Krabby Pics", ROMX
-KrabbyFrontpic: INCBIN "gfx/pokemon/krabby/front.animated.2bpp.lz"
-KrabbyBackpic:  INCBIN "gfx/pokemon/krabby/back.2bpp.lz"
+SECTION "Corphish Pics", ROMX
+CorphishFrontpic: INCBIN "gfx/pokemon/corphish/front.animated.2bpp.lz"
+CorphishBackpic:  INCBIN "gfx/pokemon/corphish/back.2bpp.lz"
 
-SECTION "Kingler Pics", ROMX
-KinglerFrontpic: INCBIN "gfx/pokemon/kingler/front.animated.2bpp.lz"
-KinglerBackpic:  INCBIN "gfx/pokemon/kingler/back.2bpp.lz"
+SECTION "Crawdaunt Pics", ROMX
+CrawdauntFrontpic: INCBIN "gfx/pokemon/crawdaunt/front.animated.2bpp.lz"
+CrawdauntBackpic:  INCBIN "gfx/pokemon/crawdaunt/back.2bpp.lz"
 
 SECTION "Exeggcute Pics", ROMX
 ExeggcuteFrontpic: INCBIN "gfx/pokemon/exeggcute/front.animated.2bpp.lz"

Pokelachia-Team/Echoes

gfx/minis_icons.asm
@@ -492,15 +492,15 @@ HypnoMini::     INCBIN "gfx/minis/hypno.2bpp.lzp"
 HypnoMiniMask:: INCBIN "gfx/minis/hypno_mask.1bpp.lzp"
 HypnoIcon::     INCBIN "gfx/icons/hypno.2bpp.lzp"
 
-SECTION "Krabby Mini Icon", ROMX
-KrabbyMini::     INCBIN "gfx/minis/krabby.2bpp.lzp"
-KrabbyMiniMask:: INCBIN "gfx/minis/krabby_mask.1bpp.lzp"
-KrabbyIcon::     INCBIN "gfx/icons/krabby.2bpp.lzp"
+SECTION "Corphish Mini Icon", ROMX
+CorphishMini::     INCBIN "gfx/minis/corphish.2bpp.lzp"
+CorphishMiniMask:: INCBIN "gfx/minis/corphish_mask.1bpp.lzp"
+CorphishIcon::     INCBIN "gfx/icons/corphish.2bpp.lzp"
 
-SECTION "Kingler Mini Icon", ROMX
-KinglerMini::     INCBIN "gfx/minis/kingler.2bpp.lzp"
-KinglerMiniMask:: INCBIN "gfx/minis/kingler_mask.1bpp.lzp"
-KinglerIcon::     INCBIN "gfx/icons/kingler.2bpp.lzp"
+SECTION "Crawdaunt Mini Icon", ROMX
+CrawdauntMini::     INCBIN "gfx/minis/crawdaunt.2bpp.lzp"
+CrawdauntMiniMask:: INCBIN "gfx/minis/crawdaunt_mask.1bpp.lzp"
+CrawdauntIcon::     INCBIN "gfx/icons/crawdaunt.2bpp.lzp"
 
 SECTION "Voltorb Mini Icon", ROMX
 VoltorbMini::     INCBIN "gfx/minis/voltorb.2bpp.lzp"

gfx/pokemon.asm
@@ -368,15 +368,15 @@ HypnoFrontpic: INCBIN "gfx/pokemon/hypno/front.animated.2bpp.lzp"
 HypnoBackpic:  INCBIN "gfx/pokemon/hypno/back.2bpp.lzp"
 HypnoFrames:   INCLUDE "gfx/pokemon/hypno/frames.asm"
 
-SECTION "Krabby Pics", ROMX
-KrabbyFrontpic: INCBIN "gfx/pokemon/krabby/front.animated.2bpp.lzp"
-KrabbyBackpic:  INCBIN "gfx/pokemon/krabby/back.2bpp.lzp"
-KrabbyFrames:   INCLUDE "gfx/pokemon/krabby/frames.asm"
+SECTION "Corphish Pics", ROMX
+CorphishFrontpic: INCBIN "gfx/pokemon/corphish/front.animated.2bpp.lzp"
+CorphishBackpic:  INCBIN "gfx/pokemon/corphish/back.2bpp.lzp"
+CorphishFrames:   INCLUDE "gfx/pokemon/corphish/frames.asm"
 
-SECTION "Kingler Pics", ROMX
-KinglerFrontpic: INCBIN "gfx/pokemon/kingler/front.animated.2bpp.lzp"
-KinglerBackpic:  INCBIN "gfx/pokemon/kingler/back.2bpp.lzp"
-KinglerFrames:   INCLUDE "gfx/pokemon/kingler/frames.asm"
+SECTION "Crawdaunt Pics", ROMX
+CrawdauntFrontpic: INCBIN "gfx/pokemon/crawdaunt/front.animated.2bpp.lzp"
+CrawdauntBackpic:  INCBIN "gfx/pokemon/crawdaunt/back.2bpp.lzp"
+CrawdauntFrames:   INCLUDE "gfx/pokemon/crawdaunt/frames.asm"
 
 SECTION "Exeggcute Pics", ROMX
 ExeggcuteFrontpic: INCBIN "gfx/pokemon/exeggcute/front.animated.2bpp.lzp"