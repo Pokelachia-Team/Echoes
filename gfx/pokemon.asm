Rangi42/polishedcrystal

remove spinarak line, add joltik line

gfx/pokemon.asm
@@ -528,13 +528,13 @@ SECTION "Ledian Pics", ROMX
 LedianFrontpic: INCBIN "gfx/pokemon/ledian/front.animated.2bpp.lz"
 LedianBackpic:  INCBIN "gfx/pokemon/ledian/back.2bpp.lz"
 
-SECTION "Spinarak Pics", ROMX
-SpinarakFrontpic: INCBIN "gfx/pokemon/spinarak/front.animated.2bpp.lz"
-SpinarakBackpic:  INCBIN "gfx/pokemon/spinarak/back.2bpp.lz"
+SECTION "Joltik Pics", ROMX
+JoltikFrontpic: INCBIN "gfx/pokemon/joltik/front.animated.2bpp.lz"
+JoltikBackpic:  INCBIN "gfx/pokemon/joltik/back.2bpp.lz"
 
-SECTION "Ariados Pics", ROMX
-AriadosFrontpic: INCBIN "gfx/pokemon/ariados/front.animated.2bpp.lz"
-AriadosBackpic:  INCBIN "gfx/pokemon/ariados/back.2bpp.lz"
+SECTION "Galvantula Pics", ROMX
+GalvantulaFrontpic: INCBIN "gfx/pokemon/galvantula/front.animated.2bpp.lz"
+GalvantulaBackpic:  INCBIN "gfx/pokemon/galvantula/back.2bpp.lz"
 
 SECTION "Crobat Pics", ROMX
 CrobatFrontpic: INCBIN "gfx/pokemon/crobat/front.animated.2bpp.lz"

gfx/pokemon/footprints.asm
@@ -3,7 +3,7 @@ Footprints:: ; needed for BANK(Footprints)
 VenomothFootprint::
 FliklitFootprint::
 KabutoFootprint::
-SpinarakFootprint:: INCBIN "gfx/footprints/circle_small.1bpp.lz"
+JoltikFootprint:: INCBIN "gfx/footprints/circle_small.1bpp.lz"
 
 CaterpieFootprint::
 WeedleFootprint::
@@ -206,7 +206,7 @@ HoothootFootprint:: INCBIN "gfx/footprints/hoothoot.1bpp.lz"
 NoctowlFootprint:: INCBIN "gfx/footprints/noctowl.1bpp.lz"
 LedybaFootprint:: INCBIN "gfx/footprints/ledyba.1bpp.lz"
 LedianFootprint:: INCBIN "gfx/footprints/ledian.1bpp.lz"
-AriadosFootprint:: INCBIN "gfx/footprints/ariados.1bpp.lz"
+GalvantulaFootprint:: INCBIN "gfx/footprints/galvantula.1bpp.lz"
 PichuFootprint:: INCBIN "gfx/footprints/pichu.1bpp.lz"
 CleffaFootprint:: INCBIN "gfx/footprints/cleffa.1bpp.lz"
 IgglybuffFootprint:: INCBIN "gfx/footprints/igglybuff.1bpp.lz"

Pokelachia-Team/Echoes

gfx/pokemon.asm
@@ -660,15 +660,15 @@ LedianFrontpic: INCBIN "gfx/pokemon/ledian/front.animated.2bpp.lzp"
 LedianBackpic:  INCBIN "gfx/pokemon/ledian/back.2bpp.lzp"
 LedianFrames:   INCLUDE "gfx/pokemon/ledian/frames.asm"
 
-SECTION "Spinarak Pics", ROMX
-SpinarakFrontpic: INCBIN "gfx/pokemon/spinarak/front.animated.2bpp.lzp"
-SpinarakBackpic:  INCBIN "gfx/pokemon/spinarak/back.2bpp.lzp"
-SpinarakFrames:   INCLUDE "gfx/pokemon/spinarak/frames.asm"
+SECTION "Joltik Pics", ROMX
+JoltikFrontpic: INCBIN "gfx/pokemon/joltik/front.animated.2bpp.lzp"
+JoltikBackpic:  INCBIN "gfx/pokemon/joltik/back.2bpp.lzp"
+JoltikFrames:   INCLUDE "gfx/pokemon/joltik/frames.asm"
 
-SECTION "Ariados Pics", ROMX
-AriadosFrontpic: INCBIN "gfx/pokemon/ariados/front.animated.2bpp.lzp"
-AriadosBackpic:  INCBIN "gfx/pokemon/ariados/back.2bpp.lzp"
-AriadosFrames:   INCLUDE "gfx/pokemon/ariados/frames.asm"
+SECTION "Galvantula Pics", ROMX
+GalvantulaFrontpic: INCBIN "gfx/pokemon/galvantula/front.animated.2bpp.lzp"
+GalvantulaBackpic:  INCBIN "gfx/pokemon/galvantula/back.2bpp.lzp"
+GalvantulaFrames:   INCLUDE "gfx/pokemon/galvantula/frames.asm"
 
 SECTION "Crobat Pics", ROMX
 CrobatFrontpic: INCBIN "gfx/pokemon/crobat/front.animated.2bpp.lzp"

gfx/pokemon/footprints.asm
@@ -3,7 +3,7 @@ Footprints:: ; needed for BANK(Footprints)
 VenomothFootprint::
 FliklitFootprint::
 KabutoFootprint::
-SpinarakFootprint:: INCBIN "gfx/footprints/circle_small.1bpp.lzp"
+JoltikFootprint:: INCBIN "gfx/footprints/circle_small.1bpp.lzp"
 
 CaterpieFootprint::
 WeedleFootprint::
@@ -208,7 +208,7 @@ HoothootFootprint:: INCBIN "gfx/footprints/hoothoot.1bpp.lzp"
 NoctowlFootprint:: INCBIN "gfx/footprints/noctowl.1bpp.lzp"
 LedybaFootprint:: INCBIN "gfx/footprints/ledyba.1bpp.lzp"
 LedianFootprint:: INCBIN "gfx/footprints/ledian.1bpp.lzp"
-AriadosFootprint:: INCBIN "gfx/footprints/ariados.1bpp.lzp"
+GalvantulaFootprint:: INCBIN "gfx/footprints/galvantula.1bpp.lzp"
 PichuFootprint:: INCBIN "gfx/footprints/pichu.1bpp.lzp"
 CleffaFootprint:: INCBIN "gfx/footprints/cleffa.1bpp.lzp"
 IgglybuffFootprint:: INCBIN "gfx/footprints/igglybuff.1bpp.lzp"