highly-curious/Project-Pokelachia

remove machop line/add timburr line

gfx/minis_icons.asm
@@ -332,20 +332,20 @@ AlakazamMini::     INCBIN "gfx/minis/alakazam.2bpp.lz"
 AlakazamMiniMask:: INCBIN "gfx/minis/alakazam_mask.1bpp.lz"
 AlakazamIcon::     INCBIN "gfx/icons/alakazam.2bpp.lz"
 
-SECTION "Machop Mini Icon", ROMX
-MachopMini::     INCBIN "gfx/minis/machop.2bpp.lz"
-MachopMiniMask:: INCBIN "gfx/minis/machop_mask.1bpp.lz"
-MachopIcon::     INCBIN "gfx/icons/machop.2bpp.lz"
+SECTION "Timburr Mini Icon", ROMX
+TimburrMini::     INCBIN "gfx/minis/timburr.2bpp.lz"
+TimburrMiniMask:: INCBIN "gfx/minis/timburr_mask.1bpp.lz"
+TimburrIcon::     INCBIN "gfx/icons/timburr.2bpp.lz"
 
-SECTION "Machoke Mini Icon", ROMX
-MachokeMini::     INCBIN "gfx/minis/machoke.2bpp.lz"
-MachokeMiniMask:: INCBIN "gfx/minis/machoke_mask.1bpp.lz"
-MachokeIcon::     INCBIN "gfx/icons/machoke.2bpp.lz"
+SECTION "Gurdurr Mini Icon", ROMX
+GurdurrMini::     INCBIN "gfx/minis/gurdurr.2bpp.lz"
+GurdurrMiniMask:: INCBIN "gfx/minis/gurdurr_mask.1bpp.lz"
+GurdurrIcon::     INCBIN "gfx/icons/gurdurr.2bpp.lz"
 
-SECTION "Machamp Mini Icon", ROMX
-MachampMini::     INCBIN "gfx/minis/machamp.2bpp.lz"
-MachampMiniMask:: INCBIN "gfx/minis/machamp_mask.1bpp.lz"
-MachampIcon::     INCBIN "gfx/icons/machamp.2bpp.lz"
+SECTION "Conkeldurr Mini Icon", ROMX
+ConkeldurrMini::     INCBIN "gfx/minis/conkeldurr.2bpp.lz"
+ConkeldurrMiniMask:: INCBIN "gfx/minis/conkeldurr_mask.1bpp.lz"
+ConkeldurrIcon::     INCBIN "gfx/icons/conkeldurr.2bpp.lz"
 
 SECTION "Bellsprout Mini Icon", ROMX
 BellsproutMini::     INCBIN "gfx/minis/bellsprout.2bpp.lz"

maps/OlsteetonAbandonedMill.asm
@@ -19,7 +19,7 @@ OlsteetonAbandonedMill_MapScriptHeader:
 	object_event  3, 12, SPRITE_BLACK_BELT, SPRITEMOVEDATA_WALK_UP_DOWN, 1, 0, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, OlsteetonAbandonedMillBlackBelt1Text, -1
 	object_event 11,  5, SPRITE_ENGINEER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, OlsteetonAbandonedMillBlackBelt2Text, -1
 	object_event 15, 12, SPRITE_FIREBREATHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 0, 1, -1, PAL_NPC_BLUE, OBJECTTYPE_COMMAND, jumptextfaceplayer, OlsteetonAbandonedMillBlackBelt3Text, -1
-	pokemon_event  2,  5, MACHOKE, SPRITEMOVEDATA_POKEMON, -1, PAL_NPC_GRAY, OlsteetonAbandonedMillMachokeText, -1
+	pokemon_event  2,  5, GURDURR, SPRITEMOVEDATA_POKEMON, -1, PAL_NPC_GRAY, OlsteetonAbandonedMillGurdurrText, -1
 
 OlsteetonAbandonedMillBlackBelt1Text:
 	text "We're not"
@@ -62,6 +62,6 @@ OlsteetonAbandonedMillBlackBelt3Text:
 	line "want it!"
 	done
 
-OlsteetonAbandonedMillMachokeText:
-	text "Machoke: Maaacho!"
+OlsteetonAbandonedMillGurdurrText:
+	text "Gurdurr: Maaacho!"
 	done

Pokelachia-Team/Echoes

gfx/minis_icons.asm
@@ -332,20 +332,20 @@ AlakazamMini::     INCBIN "gfx/minis/alakazam.2bpp.lzp"
 AlakazamMiniMask:: INCBIN "gfx/minis/alakazam_mask.1bpp.lzp"
 AlakazamIcon::     INCBIN "gfx/icons/alakazam.2bpp.lzp"
 
-SECTION "Machop Mini Icon", ROMX
-MachopMini::     INCBIN "gfx/minis/machop.2bpp.lzp"
-MachopMiniMask:: INCBIN "gfx/minis/machop_mask.1bpp.lzp"
-MachopIcon::     INCBIN "gfx/icons/machop.2bpp.lzp"
+SECTION "Timburr Mini Icon", ROMX
+TimburrMini::     INCBIN "gfx/minis/timburr.2bpp.lzp"
+TimburrMiniMask:: INCBIN "gfx/minis/timburr_mask.1bpp.lzp"
+TimburrIcon::     INCBIN "gfx/icons/timburr.2bpp.lzp"
 
-SECTION "Machoke Mini Icon", ROMX
-MachokeMini::     INCBIN "gfx/minis/machoke.2bpp.lzp"
-MachokeMiniMask:: INCBIN "gfx/minis/machoke_mask.1bpp.lzp"
-MachokeIcon::     INCBIN "gfx/icons/machoke.2bpp.lzp"
+SECTION "Gurdurr Mini Icon", ROMX
+GurdurrMini::     INCBIN "gfx/minis/gurdurr.2bpp.lzp"
+GurdurrMiniMask:: INCBIN "gfx/minis/gurdurr_mask.1bpp.lzp"
+GurdurrIcon::     INCBIN "gfx/icons/gurdurr.2bpp.lzp"
 
-SECTION "Machamp Mini Icon", ROMX
-MachampMini::     INCBIN "gfx/minis/machamp.2bpp.lzp"
-MachampMiniMask:: INCBIN "gfx/minis/machamp_mask.1bpp.lzp"
-MachampIcon::     INCBIN "gfx/icons/machamp.2bpp.lzp"
+SECTION "Conkeldurr Mini Icon", ROMX
+ConkeldurrMini::     INCBIN "gfx/minis/conkeldurr.2bpp.lzp"
+ConkeldurrMiniMask:: INCBIN "gfx/minis/conkeldurr_mask.1bpp.lzp"
+ConkeldurrIcon::     INCBIN "gfx/icons/conkeldurr.2bpp.lzp"
 
 SECTION "Bellsprout Mini Icon", ROMX
 BellsproutMini::     INCBIN "gfx/minis/bellsprout.2bpp.lzp"

maps/OlsteetonAbandonedMill.asm
@@ -19,7 +19,7 @@ OlsteetonAbandonedMill_MapScriptHeader:
 	object_event  3, 12, SPRITE_BLACK_BELT, SPRITEMOVEDATA_WALK_UP_DOWN, 1, 0, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, OlsteetonAbandonedMillBlackBelt1Text, -1
 	object_event 11,  5, SPRITE_ENGINEER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, OlsteetonAbandonedMillBlackBelt2Text, -1
 	object_event 15, 12, SPRITE_FIREBREATHER, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 0, 1, -1, PAL_NPC_BLUE, OBJECTTYPE_COMMAND, jumptextfaceplayer, OlsteetonAbandonedMillBlackBelt3Text, -1
-	; pokemon_event  2,  5, GURDURR, SPRITEMOVEDATA_POKEMON, -1, PAL_MON_GRAY, OlsteetonAbandonedMillGurdurrText, -1
+	pokemon_event  2,  5, GURDURR, SPRITEMOVEDATA_POKEMON, -1, PAL_MON_GRAY, OlsteetonAbandonedMillGurdurrText, -1
 
 OlsteetonAbandonedMillBlackBelt1Text:
 	text "We're not"
@@ -62,6 +62,6 @@ OlsteetonAbandonedMillBlackBelt3Text:
 	line "want it!"
 	done
 
-; OlsteetonAbandonedMillGurdurrText:
-; 	text "Gurdurr: Maaacho!"
-; 	done
+OlsteetonAbandonedMillGurdurrText:
+	text "Gurdurr: Gurrr!"
+	done