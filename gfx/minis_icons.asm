highly-curious/Project-Pokelachia

remove shuckle/add doozee

gfx/minis_icons.asm
@@ -1067,10 +1067,10 @@ ScizorMini::     INCBIN "gfx/minis/scizor.2bpp.lz"
 ScizorMiniMask:: INCBIN "gfx/minis/scizor_mask.1bpp.lz"
 ScizorIcon::     INCBIN "gfx/icons/scizor.2bpp.lz"
 
-SECTION "Shuckle Mini Icon", ROMX
-ShuckleMini::     INCBIN "gfx/minis/shuckle.2bpp.lz"
-ShuckleMiniMask:: INCBIN "gfx/minis/shuckle_mask.1bpp.lz"
-ShuckleIcon::     INCBIN "gfx/icons/shuckle.2bpp.lz"
+SECTION "Doozee Mini Icon", ROMX
+DoozeeMini::     INCBIN "gfx/minis/doozee.2bpp.lz"
+DoozeeMiniMask:: INCBIN "gfx/minis/doozee_mask.1bpp.lz"
+DoozeeIcon::     INCBIN "gfx/icons/doozee.2bpp.lz"
 
 SECTION "Heracross Mini Icon", ROMX
 HeracrossMini::     INCBIN "gfx/minis/heracross.2bpp.lz"

gfx/pokemon.asm
@@ -688,9 +688,9 @@ SECTION "Scizor Pics", ROMX
 ScizorFrontpic: INCBIN "gfx/pokemon/scizor/front.animated.2bpp.lz"
 ScizorBackpic:  INCBIN "gfx/pokemon/scizor/back.2bpp.lz"
 
-SECTION "Shuckle Pics", ROMX
-ShuckleFrontpic: INCBIN "gfx/pokemon/shuckle/front.animated.2bpp.lz"
-ShuckleBackpic:  INCBIN "gfx/pokemon/shuckle/back.2bpp.lz"
+SECTION "Doozee Pics", ROMX
+DoozeeFrontpic: INCBIN "gfx/pokemon/doozee/front.animated.2bpp.lz"
+DoozeeBackpic:  INCBIN "gfx/pokemon/doozee/back.2bpp.lz"
 
 SECTION "Heracross Pics", ROMX
 HeracrossFrontpic: INCBIN "gfx/pokemon/heracross/front.animated.2bpp.lz"

Pokelachia-Team/Echoes

gfx/minis_icons.asm
@@ -1067,10 +1067,10 @@ ScizorMini::     INCBIN "gfx/minis/scizor.2bpp.lzp"
 ScizorMiniMask:: INCBIN "gfx/minis/scizor_mask.1bpp.lzp"
 ScizorIcon::     INCBIN "gfx/icons/scizor.2bpp.lzp"
 
-SECTION "Shuckle Mini Icon", ROMX
-ShuckleMini::     INCBIN "gfx/minis/shuckle.2bpp.lzp"
-ShuckleMiniMask:: INCBIN "gfx/minis/shuckle_mask.1bpp.lzp"
-ShuckleIcon::     INCBIN "gfx/icons/shuckle.2bpp.lzp"
+SECTION "Doozee Mini Icon", ROMX
+DoozeeMini::     INCBIN "gfx/minis/doozee.2bpp.lzp"
+DoozeeMiniMask:: INCBIN "gfx/minis/doozee_mask.1bpp.lzp"
+DoozeeIcon::     INCBIN "gfx/icons/doozee.2bpp.lzp"
 
 SECTION "Heracross Mini Icon", ROMX
 HeracrossMini::     INCBIN "gfx/minis/heracross.2bpp.lzp"

gfx/pokemon.asm
@@ -860,10 +860,10 @@ ScizorFrontpic: INCBIN "gfx/pokemon/scizor/front.animated.2bpp.lzp"
 ScizorBackpic:  INCBIN "gfx/pokemon/scizor/back.2bpp.lzp"
 ScizorFrames:   INCLUDE "gfx/pokemon/scizor/frames.asm"
 
-SECTION "Shuckle Pics", ROMX
-ShuckleFrontpic: INCBIN "gfx/pokemon/shuckle/front.animated.2bpp.lzp"
-ShuckleBackpic:  INCBIN "gfx/pokemon/shuckle/back.2bpp.lzp"
-ShuckleFrames:   INCLUDE "gfx/pokemon/shuckle/frames.asm"
+SECTION "Doozee Pics", ROMX
+DoozeeFrontpic: INCBIN "gfx/pokemon/doozee/front.animated.2bpp.lzp"
+DoozeeBackpic:  INCBIN "gfx/pokemon/doozee/back.2bpp.lzp"
+DoozeeFrames:   INCLUDE "gfx/pokemon/doozee/frames.asm"
 
 SECTION "Heracross Pics", ROMX
 HeracrossFrontpic: INCBIN "gfx/pokemon/heracross/front.animated.2bpp.lzp"