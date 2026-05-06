71bb809dbc5a17e434d04d00efab373e181e985e
highly-curious <highly.curious@outlook.com>
2026-05-06 12:27:24 -0400

    added wild encounters

--- data/wild/johto_grass.asm
+++ data/wild/johto_grass.asm
@@ -1,3 +1,167 @@
-; Johto Pokémon in grass
+; Folkora Pokémon in grass
+	def_grass_wildmons ROUTE_101
+	db 10 percent ;encounter rate
+	; morn
+	wildmon 2, MEOWTH
+	wildmon 2, CRIBNAL
+	wildmon 3, MEOWTH
+	wildmon 3, CRIBNAL
+	wildmon 4, TIDIPOSS
+	wildmon 5, MEOWTH
+	wildmon 6, MEOWTH
+	; day
+	wildmon 2, MEOWTH
+	wildmon 2, CRIBNAL
+	wildmon 3, MEOWTH
+	wildmon 3, CRIBNAL
+	wildmon 4, TIDIPOSS
+	wildmon 5, TIDIPOSS
+	wildmon 6, MEOWTH
+	; nite
+	wildmon 3, HOOTHOOT
+	wildmon 4, TIDIPOSS
+	wildmon 3, MURKROW
+	wildmon 5, TIDIPOSS
+	wildmon 4, MURKROW
+	wildmon 6, MEOWTH
+	wildmon 10, NOCTOWL
+	end_grass_wildmons
 
+	def_grass_wildmons ROUTE_101_CAVE
+	db 10 percent ; encounter rate
+	; morn
+	wildmon 2, TEDDIURSA
+	wildmon 2, ZUBAT
+	wildmon 3, TEDDIURSA
+	wildmon 3, GEODUDE
+	wildmon 4, TEDDIURSA
+	wildmon 4, ZUBAT
+	wildmon 5, TEDDIURSA
+	; day
+	wildmon 2, TEDDIURSA
+	wildmon 2, ZUBAT
+	wildmon 3, TEDDIURSA
+	wildmon 3, GEODUDE
+	wildmon 4, TEDDIURSA
+	wildmon 4, GEODUDE
+	wildmon 5, TEDDIURSA
+	; nite
+	wildmon 2, TEDDIURSA
+	wildmon 2, ZUBAT
+	wildmon 3, TEDDIURSA
+	wildmon 3, GEODUDE
+	wildmon 4, TEDDIURSA
+	wildmon 4, ZUBAT
+	wildmon 5, TEDDIURSA
+	end_grass_wildmons
+
+	def_grass_wildmons ROUTE_102
+	db 10 percent ; encounter rate
+	wildmon 2, MEOWTH
+	wildmon 2, CRIBNAL
+	wildmon 3, MEOWTH
+	wildmon 3, CRIBNAL
+	wildmon 4, LEDYBA
+	wildmon 5, MEOWTH
+	wildmon 6, TIDIPOSS
+	; day
+	wildmon 2, MEOWTH
+	wildmon 2, CRIBNAL
+	wildmon 3, MEOWTH
+	wildmon 3, CRIBNAL
+	wildmon 4, LEDYBA
+	wildmon 5, MEOWTH
+	wildmon 6, TIDIPOSS
+	; nite
+	wildmon 3, HOOTHOOT
+	wildmon 4, TIDIPOSS
+	wildmon 3, MURKROW
+	wildmon 5, TIDIPOSS
+	wildmon 3, MURKROW
+	wildmon 6, TIDIPOSS
+	wildmon 10, NOCTOWL
+	end_grass_wildmons
+
+	def_grass_wildmons OLSTEETON
+	db 10 percent ; encounter rate
+	wildmon 2, SINISTEA
+	wildmon 2, SINISTEA
+	wildmon 3, SINISTEA
+	wildmon 3, SINISTEA
+	wildmon 4, SINISTEA
+	wildmon 5, SINISTEA
+	wildmon 6, SINISTEA
+	; day
+	wildmon 2, SINISTEA
+	wildmon 2, SINISTEA
+	wildmon 3, SINISTEA
+	wildmon 3, SINISTEA
+	wildmon 4, SINISTEA
+	wildmon 5, SINISTEA
+	wildmon 6, SINISTEA
+	; nite
+	wildmon 3, DOOZEE
+	wildmon 4, DOOZEE
+	wildmon 3, DOOZEE
+	wildmon 5, DOOZEE
+	wildmon 3, DOOZEE
+	wildmon 6, DOOZEE
+	wildmon 10, DOOZEE
+	end_grass_wildmons
+
+	def_grass_wildmons NATIONAL_FOREST_1
+	db 10 percent ; encounter rate
+	wildmon 2, CATERPIE
+	wildmon 2, CRIBNAL
+	wildmon 3, CATERPIE
+	wildmon 3, COTTONEE
+	wildmon 4, CRIBNAL
+	wildmon 5, NOFAWTS
+	wildmon 6, STANTLER
+	; day
+	wildmon 2, WEEDLE
+	wildmon 2, CRIBNAL
+	wildmon 3, NOFAWTS
+	wildmon 3, STANTLER
+	wildmon 4, COTTONEE
+	wildmon 5, NOFAWTS
+	wildmon 6, TIDIPOSS
+	; nite
+	wildmon 3, HOOTHOOT
+	wildmon 4, WEEDLE
+	wildmon 3, MURKROW
+	wildmon 5, TIDIPOSS
+	wildmon 3, MURKROW
+	wildmon 6, TIDIPOSS
+	wildmon 10, NOCTOWL
+	end_grass_wildmons
 	db -1 ; end
+
+def_grass_wildmons OLSTEETON_ABANDONED_MILL_OUTSIDE
+	db 10 percent ; encounter rate
+	wildmon 2, DEBRITE
+	wildmon 2, MEOWTH, GALARIAN_FORM
+	wildmon 3, DEBRITE
+	wildmon 3, MEOWTH, GALARIAN_FORM
+	wildmon 3, SLUGMA
+	wildmon 5, SLUGMA
+	wildmon 6, SKARMORY
+	; day
+	wildmon 2, DEBRITE
+	wildmon 2, SLUGMA
+	wildmon 3, SLUGMA
+	wildmon 3, TIMBURR
+	wildmon 4, DEBRITE
+	wildmon 5, TIMBURR
+	wildmon 6, SKARMORY
+	; nite
+	wildmon 2, DEBRITE
+	wildmon 2, MEOWTH, GALARIAN_FORM
+	wildmon 3, SLUGMA
+	wildmon 3, MEOWTH, GALARIAN_FORM
+	wildmon 3, DEBRITE
+	wildmon 5, MEOWTH, GALARIAN_FORM
+	wildmon 6, SKARMORY
+	end_grass_wildmons
+	db -1 ; end
+
