259281679cf21ca54336fd37ba124441bde067e4
highly-curious <highly.curious@outlook.com>
2026-07-16 16:05:08 -0400

    brinesburg gym fixes & add magikorp event

--- maps/Brinesburg.asm
+++ maps/Brinesburg.asm
@@ -1,14 +1,17 @@
 Brinesburg_MapScriptHeader:
 	def_scene_scripts
+	scene_const SCENE_BRINESBURG_NOOP
+	scene_const SCENE_BRINESBURG_MAGIGOON
+
 
 	def_callbacks
 
 	def_warp_events
 	warp_event  6, 28, ROUTE_103_BRINESBURG_GATE, 3
 	warp_event  6, 29, ROUTE_103_BRINESBURG_GATE, 4
-	warp_event 53, 11, BRINESBURG_POKECENTER, 1
+	warp_event 55, 11, BRINESBURG_POKECENTER, 1
 	warp_event 47, 19, BRINESBURG_MART, 1
-	warp_event 34, 19, BRINESBURG_GYM, 2
+	warp_event 34, 19, BRINESBURG_GYM, 1
 	warp_event 57,  3, SALT_BRINE_CAVE, 1
 	warp_event 11, 23, BRINESBURG_POLLUTION_SPEECH_HOUSE, 1
 	warp_event 45, 33, BRINESBURG_NICKNAME_SPEECH_HOUSE, 1
@@ -18,13 +21,15 @@ Brinesburg_MapScriptHeader:
 	warp_event 19,  3, BRINESBURG_FEUD_RIGHT_HOUSE, 1
 	warp_event  6, 12, BRINESBURG_TRAIN_STATION, 1
 	warp_event  6, 13, BRINESBURG_TRAIN_STATION, 2
-	warp_event 45, 13, BRINESBURG_SALT_SHOP, 1
-	warp_event 55, 21, BRINESBURG_MAGIKORP_OFFICE, 1
+	warp_event 47, 13, BRINESBURG_SALT_SHOP, 1
+	warp_event 55, 21, BRINESBURG_MAGIKORP_OFFICE, 2
 	warp_event 55, 30, BRINESBURG_ROUTE_105_GATE, 1
-	warp_event 55, 31, BRINESBURG_ROUTE_105_GATE, 2 
+	warp_event 55, 31, BRINESBURG_ROUTE_105_GATE, 2
 
 
 	def_coord_events
+	coord_event 50, 20, SCENE_BRINESBURG_MAGIGOON, Brinesburg_MagigoonTrigger1
+	coord_event 50, 21, SCENE_BRINESBURG_MAGIGOON, Brinesburg_MagigoonTrigger2
 
 	def_bg_events
 	bg_event 19, 26, BGEVENT_JUMPTEXT, BrinesburgSignText
@@ -37,7 +42,7 @@ Brinesburg_MapScriptHeader:
 	bg_event 33, 30, BGEVENT_JUMPTEXT, BrinesburgPotholeText
 	bg_event 23,  8, BGEVENT_JUMPTEXT, BrinesburgPotholeText
 	bg_event 51,  8, BGEVENT_JUMPTEXT, BrinesburgPotholeText
-	bg_event 55, 14, BGEVENT_JUMPTEXT, BrinesburgPotholeText
+	bg_event 53, 16, BGEVENT_JUMPTEXT, BrinesburgPotholeText
 	bg_event  7, 11, BGEVENT_JUMPTEXT, BrinesburgTrainStationSignText
 	bg_event 39, 17, BGEVENT_JUMPTEXT, BrinesburgAdvancedTipsSignText
 	bg_event 55, 29, BGEVENT_JUMPTEXT, BrinesburgRoute105SignText
@@ -46,47 +51,52 @@ Brinesburg_MapScriptHeader:
 	bg_event  5, 24, BGEVENT_ITEM + FULL_HEAL, EVENT_BRINESBURG_HIDDEN_FULL_HEAL
 
 	def_object_events
-	object_event 27, 32, SPRITE_BATTLE_GIRL, SPRITEMOVEDATA_WANDER, 1, 1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgTeacherText, -1
+	object_event 52, 21, SPRITE_ROCKET, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 0, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, ObjectEvent, EVENT_FOUGHT_FIELD_LAB_MAGIGOON
+	object_event 41, 32, SPRITE_BATTLE_GIRL, SPRITEMOVEDATA_WANDER, 1, 1, -1, PAL_NPC_RED, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgTeacherText, -1
 	object_event 40, 21, SPRITE_ROCKER, SPRITEMOVEDATA_WANDER, 1, 1, -1, PAL_NPC_GREEN, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgSuperNerdText, -1
 	object_event 29, 11, SPRITE_SAILOR, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 0, 3, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgSailorText, -1
 	object_event 31, 26, SPRITE_FISHER, SPRITEMOVEDATA_STANDING_RIGHT, 0, 3, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgFisherText, -1
 	object_event 18, 34, SPRITE_COOL_DUDE, SPRITEMOVEDATA_STANDING_UP, 0, 3, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgCoolDudeText, -1
 	object_event 19, 34, SPRITE_POKEMANIAC, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 3, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgPokemaniacText, -1
 	object_event 20, 34, SPRITE_PICNICKER, SPRITEMOVEDATA_STANDING_LEFT, 0, 3, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgPokemaniacText, -1
-	object_event 16, 14, SPRITE_SAILOR, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 0, 3, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgSailorText, -1
+	object_event 16, 13, SPRITE_SAILOR, SPRITEMOVEDATA_WALK_LEFT_RIGHT, 0, 2, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgSailorText, -1
 	object_event 44,  1, SPRITE_ENGINEER, SPRITEMOVEDATA_SPINRANDOM_SLOW, 0, 3, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgConstructionText, -1
 	object_event 47,  1, SPRITE_ENGINEER, SPRITEMOVEDATA_STANDING_DOWN, 0, 3, -1, 0, OBJECTTYPE_COMMAND, jumptextfaceplayer, BrinesburgConstructionText, -1
 	pokemon_event 45,  1, TIMBURR, SPRITEMOVEDATA_POKEMON, -1, PAL_MON_BROWN, BrinesburgTimburrText, -1
 	pokemon_event 46,  1, GURDURR, SPRITEMOVEDATA_POKEMON, -1, PAL_MON_BROWN, BrinesburgGurdurrText, -1
 
-
-
 	; cuttree_event  5, 22, EVENT_BRINESBURG_CITY_CUT_TREE
 
 	object_const_def
+	const BRINESBURG_ROCKET
 
-; BrinesburgSnorlax:
-; 	opentext
-; 	special SpecialSnorlaxAwake
-; 	iftruefwd .Awake
-; 	jumpthisopenedtext
 
-; 	text "Snorlax is snoring"
-; 	line "peacefully…"
-; 	done
+Brinesburg_MagigoonTrigger1:
+Brinesburg_MagigoonTrigger2:
+	turnobject BRINESBURG_ROCKET, LEFT
+	showemote EMOTE_SHOCK, BRINESBURG_ROCKET, 15
+	showtext Text_ArceusDammit
+	applymovement BRINESBURG_ROCKET, .MagigoonRunSteps
+	playsound SFX_EXIT_BUILDING
+	disappear BRINESBURG_ROCKET
+	setscene SCENE_BRINESBURG_NOOP
+	setevent EVENT_FOUGHT_FIELD_LAB_MAGIGOON
+	clearevent EVENT_MET_GIL
+	setmapscene BRINESBURG_MAGIKORP_OFFICE, SCENE_BRINESBURG_MAGIKORP_OFFICE_FIRED_MAGIGOON
+	end
 
-; .Awake:
-; 	writetext BrinesburgRadioNearSnorlaxText
-; 	pause 15
-; 	cry SNORLAX
-; 	closetext
-; 	loadvar VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
-; 	loadwildmon SNORLAX, 60
-; 	startbattle
-; 	disappear BRINESBURGCITY_BIG_SNORLAX
-; 	setevent EVENT_FOUGHT_SNORLAX
-; 	reloadmapafterbattle
-; 	end
+.MagigoonRunSteps:
+	big_step_down
+	big_step_down
+	big_step_right
+	big_step_right
+	big_step_right
+	step_up
+    step_end
+
+Text_ArceusDammit:
+	text "Arceus dammit!"
+	done 
 
 BrinesburgTeacherText:
 	text "Needs dialogue"
--- ram/wramx.asm
+++ ram/wramx.asm
@@ -1263,7 +1263,7 @@ wNeededMonPalLight:: db ; for SPRITE_MON_ICON two-nybble palettes, stores the li
 wNeededPalType:: db ; 0 = normal palette, non-zero = mon two-nybble palette
 wLoadedObjPalType:: db ; bitmask: bit N set = slot N is a mon palette, clear = normal palette
 
-	ds 133 ; unused
+	ds 132 ; unused
 
 wCandyAmounts::
 	table_width 1
